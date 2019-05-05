2c918809be0e847ac453df695c9eccf499f01a47
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-05-05 17:23:25 +0900

    finish #13 14

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const hbs = require("hbs")
 
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear()
+})
+hbs.registerHelper("uppercase", text => {
+  return text.toUpperCase()
+})
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
   res.render("about.hbs", {
     pageTitle: "Home page",
     content: "This is Home.hbs",
-    currentYear: new Date().getFullYear()
   })
 });
 
@@ -18,7 +23,6 @@ app.get("/about", (req, res) => {
   res.render("about.hbs", {
     pageTitle: "About page",
     content: "コンテンツ",
-    currentYear: new Date().getFullYear()
   })
 });
 app.listen(3000);
