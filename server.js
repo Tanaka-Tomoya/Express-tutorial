3300e45244016db81e096721a535eb938f4009a1
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-05-05 16:33:56 +0900

    finish #6

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", (req, res) => {
   res.send("<h1>HogeHoge</h1>");
 });
@@ -9,6 +11,6 @@ app.get("/about", (req, res) => {
   res.send({
     name: "太郎",
     age: 20
-  })
-})
+  });
+});
 app.listen(3000);
