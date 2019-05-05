eaa663cf4c60611c7c4fcf9c5d162bfe46a8f205
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-05-05 17:42:29 +0900

    finish # 15 16

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const hbs = require("hbs")
+const fs = require('fs')
 
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + '/views/partials')
@@ -11,6 +12,21 @@ hbs.registerHelper("uppercase", text => {
   return text.toUpperCase()
 })
 app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  let now = new Date();
+  let log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + "\n", err => {
+    if (err) {
+      console.log(err)
+    }
+  })
+  next();
+})
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+})
 
 app.get("/", (req, res) => {
   res.render("about.hbs", {
